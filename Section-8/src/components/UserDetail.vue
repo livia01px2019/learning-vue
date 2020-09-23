62215fa3a5875590c4c16507ca5a1f5e1f54760d
livia01px2019 <livia.zhu@live.com>
2020-09-23 18:30:21 +0800

    communicating between components

---
+++ Section-8/src/components/UserDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ switchName() }}</p>
+    <button @click="resetName">Reset Name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // can use camelcase for props here
+  props: {
+    name: {
+      type: String,
+      required: true,
+      //   default: "Livia"
+      // if type is object, default would be a function that returns an object
+    },
+  },
+  methods: {
+    switchName() {
+      return this.name.split("").reverse().join("");
+    },
+    resetName() {
+      this.myName = "Livia";
+      // emit custom event to notify parent component
+      this.$emit("nameWasReset", this.myName);
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightcoral;
+}
+</style>
