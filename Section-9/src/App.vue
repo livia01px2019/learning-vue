8d963f221fbd9ec5540694de2807bb8f68830a78
livia01px2019 <livia.zhu@live.com>
2020-09-24 15:29:20 +0800

    section 9

---
+++ Section-9/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        <hr />
+        <!-- component is destroyed and recreated new each time -->
+        <component :is="selectedComponent">
+          <h1 slot="title">{{ quoteTitle }}</h1>
+          <p>A wonderful quote</p>
+        </component>
+        <!-- component not destroyed anymore -->
+        <keep-alive>
+          <component :is="selectedComponent">
+            <h1 slot="title">{{ quoteTitle }}</h1>
+            <p>A wonderful quote</p>
+          </component>
+        </keep-alive>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote.vue";
+import Author from "./components/Author.vue";
+import New from "./components/New.vue";
+
+export default {
+  data() {
+    return {
+      quoteTitle: "The Quote",
+      selectedComponent: "appQuote",
+    };
+  },
+  components: {
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New,
+  },
+};
+</script>
+
+<style>
+</style>
