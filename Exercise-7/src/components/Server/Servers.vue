a1793841edab9509b35c0db4160aed52d43664be
livia01px2019 <livia.zhu@live.com>
2020-09-24 13:39:15 +0800

    finished exercise 7

--- Exercise-7/src/components/Server/Servers.vue
+++ Exercise-7/src/components/Server/Servers.vue
@@ -1,18 +1,56 @@
 <template>
-    <div class="col-xs-12 col-sm-6">
-        <ul class="list-group">
-            <li
-                    class="list-group-item"
-                    v-for="index in 5">
-                Server #{{ index }}
-            </li>
-        </ul>
-    </div>
+  <div class="col-xs-12 col-sm-6">
+    <ul class="list-group">
+      <server
+        v-for="s in servers"
+        :key="s.id"
+        :index="s.id"
+        :status="s.status"
+        :isActive="s.isActive"
+      ></server>
+    </ul>
+  </div>
 </template>
 
 <script>
+import Server from "./Server.vue";
+import { eventBus } from "../../main";
+
+export default {
+  data: function () {
+    return {
+      servers: [
+        { id: 1, status: "Normal", isActive: false },
+        { id: 2, status: "Critical", isActive: false },
+        { id: 3, status: "Unknown", isActive: false },
+        { id: 4, status: "Normal", isActive: false },
+      ],
+    };
+  },
+  components: {
+    server: Server,
+  },
+  created() {
+    eventBus.$on("serverStatusChanged", (data) => {
+      for (var s of this.servers) {
+        if (s.id == data.index) {
+          s.status = data.status;
+        }
+      }
+    });
+    eventBus.$on("serverClicked", (data) => {
+      for (var s of this.servers) {
+        if (s.id == data.index) {
+          s.status = data.status;
+          s.isActive = true;
+        } else {
+          s.isActive = false;
+        }
+      }
+    });
+  },
+};
 </script>
 
 <style>
-
 </style>
--- Exercise-7/src/main.js
+++ Exercise-7/src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+export const eventBus = new Vue({
+  methods: {
+    serverClicked(serverData) {
+      console.log(serverData);
+      this.$emit('serverClicked', serverData);
+    },
+    serverStatusChanged(serverData) {
+      console.log(serverData);
+      this.$emit('serverStatusChanged', serverData);
+    }
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
